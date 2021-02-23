9f45e2e5920aca17d0ce329532eb30971a1c46dc
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-02-23 23:01:59 +0100

    Adapter - implementation

---
+++ structural/adapter/client.go
@@ -0,0 +1,13 @@
+// Client code
+
+package main
+
+import "fmt"
+
+type client struct {
+}
+
+func (c *client) insertLightningConnectorIntoComputer(com computer) {
+    fmt.Println("Client inserts Lightning connector into computer.")
+    com.insertIntoLightningPort()
+}
