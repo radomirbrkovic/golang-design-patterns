53a5e7aa17c78c2260bf34f82b5e578f38d8b3fa
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-03-10 18:34:33 +0100

    Command - implementation

---
+++ behavioral/command/tv.go
@@ -0,0 +1,19 @@
+// Concrete receiver
+
+package main
+
+import "fmt"
+
+type tv struct {
+	isRunning bool
+}
+
+func (t *tv) on() {
+	t.isRunning = true
+	fmt.Println("Turing tv on")
+}
+
+func (t *tv) off()  {
+	t.isRunning = false
+	fmt.Println("Turing tv off")
+}
